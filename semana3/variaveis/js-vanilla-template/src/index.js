ce0e97de03d2283a5697245bd8b06a509206a1fa
quirinojess <qrnojess@gmail.com>
2021-02-22 14:06:27 -0300

    variáveis JS

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,70 @@
+// //a = 10
+// b = 10
+
+// console.log(b)
+
+// b = 5
+// console.log(a, b)
+
+// está armazenando uma informação na variável (a) e (b)
+// Será impresso no primeiro comando 10 e no segundo 10 e 5
+
+// a = 10
+// b = 20
+// c = a
+// b = c
+// a = b
+
+// console.log(a, b, c)
+// // está armazenando uma informação na variável (a) e (b). A variável c = valor de A (10), 
+// B foi substituida pelo valor de c (10) e a no final = b (20)
+// // Será impresso 20 | 10 | 10
+
+let nome =  undefined
+let idade =  undefined
+
+console.log(typeof(nome, idade))
+
+/* aparece indefinido pois não atribui valor */
+
+nome = 'Jessica'
+idade = 29
+console.log('Olá,', nome, 'você tem', idade, 'anos.')
+
+const cor = 'azul'
+const musica = 'beirut'
+const profissao = 'designer'
+const comida = 'rosquinha'
+const filhos = false
+
+console.log('qual sua cor favorita? R: minha cor favorita é', cor )
+console.log('qual sua banda favorita? R: minha banda favorita é', musica )
+console.log('qual sua profissão? R: eu sou', profissao )
+console.log('qual sua comida favorita? R: minha comida favorita é', comida )
+console.log('você tem filhos? R:', filhos )
+
+let comidasFavoritas = ['rolinho primavera', 'rosquinha', 'torta de maçã', 'hamburguer' ]
+console.log(comidasFavoritas)
+console.log('Essas são as minhas comidas preferidas: ')
+console.log( comidasFavoritas[0])
+console.log( comidasFavoritas[1])
+console.log( comidasFavoritas[2])
+console.log( comidasFavoritas[3])
+
+comidasFavoritas[1] = 'macarronada'
+
+console.log(comidasFavoritas)
+
+const tempo = false
+const politica = true
+const flor = true
+const doce = true
+const trabalho = false
+
+console.log('vai chover hoje? ', tempo)
+console.log('você gosta de política ', politica)
+console.log('você via a flor? ', flor)
+console.log('você comeu o doce? ', doce)
+console.log('está trabalhando? ', trabalho)
+
+
