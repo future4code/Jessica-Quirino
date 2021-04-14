8fa5a7440e6051d9b6b6bfe471624900dcd140b0
quirinojess <qrnojess@gmail.com>
2021-04-14 16:48:26 -0300

    layout labex

--- semana10/labex/src/App.js
+++ semana10/labex/src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/homePage';
+import ListTrip from './pages/listTripPage'
+import Header from './components/header'
+import { createGlobalStyle } from 'styled-components'
+import styled from 'styled-components'
+import back from './img/back.png'
+import backmobile from './img/back-mob.png'
+import "./assets/css/fonts.css";
+
+const GlobalStyle = createGlobalStyle`
+
+body{
+  background: #E7E4E7 url(${back}) no-repeat top right;
+  font-family: 'Poppins', sans-serif;}
+
+  button{
+    font-family: 'Poppins', sans-serif;
+  }
+  span{padding-top: 5px;
+    padding-bottom:5px;
+    border-bottom: 1px solid #DB4472;}
+
+    h1{font-size:30px;
+    color:white;
+  }
+
+  @media screen and (max-width: 900px) {
+  body {
+    background: #E7E4E7 url(${backmobile}) no-repeat top right;
+  }
+
+
+}
+  `
+
+const Main = styled.div`
+width:80vw;
+margin:0 auto;
+margin-top:5vh;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <GlobalStyle />     <Header/>
+    <Main>
+      <HomePage/>
+  
+      </Main>
+
+      <ListTrip/>
+  </div>
   );
 }
 
