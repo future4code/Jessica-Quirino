f71e77f69264ff7ad95f024c9bef1a5a59643482
quirinojess <qrnojess@gmail.com>
2021-05-01 11:35:49 -0300

    projeto final

--- semana12/labeddit/src/hooks/useRequestPost.js
+++ semana12/labeddit/src/hooks/useRequestPost.js
@@ -1,16 +1,17 @@
+import React, {useEffect}  from 'react'
 import axios from 'axios'
 
 const useRequestPost = (url, body, headers) => {
 
-axios.post(url,body,{headers})
-.then((res) =>{
-console.log(window.alert("Envio Concluído!"))
-body(res.data)
-})
-.catch((err) => {
-console.log(err.data)}
+useEffect(() => {
+axios
+.post(url,body,{headers})
+.then((res) =>
+{console.log(window.alert("Envio Concluído!"))
+body(res.data);}
 )
-
+.catch((err) => 
+{console.log(err.data)}
+)}, [body]);
 }
-
 export default useRequestPost
